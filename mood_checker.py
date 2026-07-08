6c05e3b1ccdb4a1d9ebb4ce7c9a420f23482d2cb
Amir Mohammad Noori <a90088885@gmail.com>
2026-07-08 22:51:33 +0330

    Create mood_checker.py

---
+++ mood_checker.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+print("=" * 30)
+print("   ✨ How are you today?")
+print("=" * 30)
+
+name = input("👤 Your name: ")
+mood = input("❤️ How are you? (good/okay/bad/excited): ")
+
+if mood == "good":
+    print("😎 " + name + "! Today is your day, go shine!")
+elif mood == "okay":
+    print("🤔 " + name + "! Take a small step, you'll get there.")
+elif mood == "excited":
+    print("🔥 " + name + "! That's the spirit! Go get 'em!")
+else:
+    print("🤗 " + name + "! Don't worry, I'm here for you!")
+  اضافه کردن برنامه حالت سنج
