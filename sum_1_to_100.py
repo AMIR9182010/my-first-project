075ba23723abd9dacdff5be672ae4a7ee906a15a
Amir Mohammad Noori <a90088885@gmail.com>
2026-07-13 15:59:37 +0330

    Create sum_1_to_100.py

---
+++ sum_1_to_100.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+print("=" * 30)
+print("   🧮 Sum of Numbers 1 to 100")
+print("=" * 30)
+
+total = 0
+
+for i in range(1, 101):
+    total = total + i
+
+print("Sum of numbers from 1 to 100 is: " + str(total))
+اضافه کردن برنامه جمع اعداد ۱ تا ۱۰۰
